b51b26e4639e1129ac69e7939846aff483dae7ba
Febriyanto1502 <febriyantobagoes@gmail.com>
2020-01-07 14:08:21 +0700

    Latihan

---
+++ Kemampuan-dasar/Kemampuan-dasar 1/Latihan1/comment_example.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Add two numeric value
+((sum=25+35))
+#print the result
+echo $sum
---
+++ Kemampuan-dasar/Kemampuan-dasar 1/Latihan1/echo_example.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "printing text with timeline"
+echo -n "printing text without newline"
+echo -e "\nRemoving \t Backlist \t character\n"
